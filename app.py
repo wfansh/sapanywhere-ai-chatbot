3c75ccb72ff25748b9fd7dcfa09d029f19e48b5c
wfansh <wfansh@gmail.com>
2016-12-21 19:15:58 +0800

    app.py
    my change

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import urllib
 import json
 import os
 
+import httplib
 from flask import Flask
 from flask import request
 from flask import make_response
@@ -12,31 +13,38 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['POST'])
+
+api_key = 'DukY4kB5FjKn4DMMbqpXiei37yo21Gnz'
+api_secret = 'PIi7Wab2wtLAEKXjDoD9W2LKzuyWwH85'
+refresh_token = '9ffbe34-b169-49e4-8b47-5cf31d7805e9'
+api_endpoint = 'https://api.sapanywhere.com/v1/'
+auth_endpoint = 'https://go.sapanywhere.com/oauth2/token'
+
+
+@app.route('/webhook', methods=['POST', 'GET'] )
 def webhook():
-    req = request.get_json(silent=True, force=True)
+    req = request.get_json(silent = True, force = True)
 
     print("Request:")
-    print(json.dumps(req, indent=4))
+    print(json.dumps(req, indent = 4))
 
     res = makeWebhookResult(req)
 
-    res = json.dumps(res, indent=4)
+    res = json.dumps(res, indent = 4)
     print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+    if req.get("result").get("action") != "lead.create":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
+    customer = parameters.get("customer")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    speech = "Create sales lead for customer " + customer
 
     print("Response:")
     print(speech)
@@ -50,9 +58,22 @@ def makeWebhookResult(req):
     }
 
 
+def httpTest():
+	http_client = httplib.HTTPConnection('127.0.0.1', 8080, timeout = 30)
+	http_client.request('GET', '/contextio-showcase')
+
+	response = http_client.getresponse()
+	print response.read()
+
+
+
 if __name__ == '__main__':
+
     port = int(os.getenv('PORT', 5000))
+
+    httpTest()
+
 
     print "Starting app on port %d" % port
 
-    app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug = True, port = port, host = '127.0.0.1')
